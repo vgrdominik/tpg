6caf7460c52e3d4b1da209de6bdcf1851e4122a8
Valentí Gàmez <vgr.gamez@gmail.com>
2020-04-15 12:13:15 +0200

    Disabled SyliusShopBundle.

--- config/bundles.php
+++ config/bundles.php
@@ -46,7 +46,8 @@ return [
     Sylius\Bundle\PayumBundle\SyliusPayumBundle::class => ['all' => true],
     Sylius\Bundle\ThemeBundle\SyliusThemeBundle::class => ['all' => true],
     Sylius\Bundle\AdminBundle\SyliusAdminBundle::class => ['all' => true],
-    Sylius\Bundle\ShopBundle\SyliusShopBundle::class => ['all' => true],
+    // Sylius default shop bundle disabled
+    // Sylius\Bundle\ShopBundle\SyliusShopBundle::class => ['all' => true],
     FOS\OAuthServerBundle\FOSOAuthServerBundle::class => ['all' => true],
     Sylius\Bundle\AdminApiBundle\SyliusAdminApiBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true, 'test_cached' => true],
